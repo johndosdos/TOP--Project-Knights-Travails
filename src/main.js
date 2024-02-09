c795407546cc56e7e3d44d8ac8dd38d3be492443
John <128350391+alpaOo@users.noreply.github.com>
2024-02-10 03:55:54 +0800

    Add chess board initialization
    - Initialize default value to 0 for every cell

--- src/main.js
+++ src/main.js
@@ -7,7 +7,11 @@
  * @param {string} destination
  */
 function knight_moves(source, destination) {
- */
+  const CHESS_BOARD = Array.from({ length: 8 }, function () {
+    return Array.from({ length: 8 }, function () {
+      return 0;
+    });
+  });
 function main() {
   knight_moves("a1", "b3");
 }
