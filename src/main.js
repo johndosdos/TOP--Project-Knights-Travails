0d4e1866e2944c6b88b40fb8fb10a368e068369f
John <128350391+alpaOo@users.noreply.github.com>
2024-02-10 03:59:51 +0800

    Add helper function to convert notation to coordinates
    - Converts chess notation's rank and file to its equivalent coordinates on the chess board array

--- src/main.js
+++ src/main.js
@@ -12,6 +12,22 @@ function knight_moves(source, destination) {
       return 0;
     });
   });
+
+  const SOURCE_COORDINATES = notation_to_array_coordinates(source);
+  const DESTINATION_COORDINATES = notation_to_array_coordinates(destination);
+//HELPER FUNCTIONS
+
+/**
+ *
+ * @param {string} notation
+ * @returns {number[]}
+ */
+function notation_to_array_coordinates(notation) {
+  const RANK = 8 - parseInt(notation[1]);
+  const FILE = notation.charCodeAt(0) - "a".charCodeAt(0);
+
+  return [RANK, FILE];
+}
 function main() {
   knight_moves("a1", "b3");
 }
