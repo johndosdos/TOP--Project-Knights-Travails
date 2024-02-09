09e22c6ca3d8472e1310e3d493341eda1611182c
John <128350391+alpaOo@users.noreply.github.com>
2024-02-10 04:57:53 +0800

    Add function to calculate valid knight moves on chessboard

--- src/main.js
+++ src/main.js
@@ -15,6 +15,14 @@ function knight_moves(source, destination) {
 
   const SOURCE_COORDINATES = notation_to_array_coordinates(source);
   const DESTINATION_COORDINATES = notation_to_array_coordinates(destination);
+
+  const VALID_KNIGHT_MOVES = get_valid_knight_moves(
+    SOURCE_COORDINATES[0],
+    SOURCE_COORDINATES[1],
+    CHESS_BOARD
+  );
+}
+
 //HELPER FUNCTIONS
 
 /**
@@ -28,6 +36,45 @@ function notation_to_array_coordinates(notation) {
 
   return [FILE, RANK];
 }
+
+/**
+ *
+ * @param {number} source_x
+ * @param {number} source_y
+ * @param {number[][]} chess_board
+ */
+function get_valid_knight_moves(source_x, source_y, chess_board) {
+  const MOVES = [
+    // 0, up-left L move
+    { source_x: source_x - 1, source_y: source_y - 2 },
+    // 1, up-right L move
+    { source_x: source_x + 1, source_y: source_y - 2 },
+    // 2, right-up L move
+    { source_x: source_x + 2, source_y: source_y - 1 },
+    // 3, right-down L move
+    { source_x: source_x + 2, source_y: source_y + 1 },
+    // 4, down-right L move
+    { source_x: source_x + 1, source_y: source_y + 2 },
+    // 5, down-left L move
+    { source_x: source_x - 1, source_y: source_y + 2 },
+    // 6, left-down L move
+    { source_x: source_x - 2, source_y: source_y + 1 },
+    // 7, left-up L move
+    { source_x: source_x - 2, source_y: source_y - 1 },
+  ];
+
+  return MOVES.filter(function (move) {
+    return (
+      move.source_x >= 0 &&
+      move.source_x < chess_board[0].length &&
+      move.source_y >= 0 &&
+      move.source_y < chess_board.length
+    );
+  });
+}
+
+/////////////////
+
 function main() {
   knight_moves("a1", "b3");
 }
